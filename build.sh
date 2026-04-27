5c43866d348cba2896dbd43ef86cea625da115a1
abdulrahoofpc <raufpc1998@gmail.com>
2026-04-27 12:55:23 +0530

    Auto migrate and create superuser on deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-# Render build script for Kavalakat Django backend
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@kavalakat.com', 'Admin@1234')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
